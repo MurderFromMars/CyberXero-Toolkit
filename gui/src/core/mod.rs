14fba08399ec434b8f053c880f52ffdbf76cdc45
MurderFromMars <untamedprodigy67@gmail.com>
2026-02-18 03:59:33 -0500

    Update mod.rs

--- gui/src/core/mod.rs
+++ gui/src/core/mod.rs
@@ -17,4 +17,3 @@ pub mod system_check;
 // Re-export commonly used items
 pub use aur::get as aur_helper;
 pub use package::{is_flatpak_installed, is_package_installed, is_package_in_repos};
-pub use system_check::check_system_requirements;

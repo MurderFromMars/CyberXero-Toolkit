d04a3d9cac4331398e2ee9341e69537acff998cf
MurderFromMars <untamedprodigy67@gmail.com>
2026-02-23 23:19:51 -0500

    Update gaming_tools.rs

--- gui/src/ui/pages/gaming_tools.rs
+++ gui/src/ui/pages/gaming_tools.rs
@@ -1,9 +1,9 @@
 //! Gaming tools page button handlers.
 //!
 //! Handles:
-//! - Steam AiO installation
+//! - Gaming suite installation (CachyOS meta or curated package list fallback)
 //! - LACT GPU overclocking
-//! - Game launchers (Lutris, Heroic, Bottles)
+//! - Game launchers (Bottles)
 //! - Controller tools
 //! - Falcond gaming utility
 
@@ -15,100 +15,134 @@ use log::info;
 
 /// Set up all button handlers for the gaming tools page.
 pub fn setup_handlers(page_builder: &Builder, _main_builder: &Builder, window: &ApplicationWindow) {
-    setup_steam_aio(page_builder, window);
+    setup_gaming_meta(page_builder, window);
     setup_lact_oc(page_builder, window);
-    setup_lutris(page_builder, window);
-    setup_heroic(page_builder, window);
     setup_bottles(page_builder, window);
     setup_controller(page_builder, window);
     setup_falcond(page_builder, window);
 }
 
-fn setup_steam_aio(builder: &Builder, window: &ApplicationWindow) {
-    let button = extract_widget::<Button>(builder, "btn_steam_aio");
+fn setup_gaming_meta(builder: &Builder, window: &ApplicationWindow) {
+    let button = extract_widget::<Button>(builder, "btn_gaming_meta");
     let window = window.clone();
 
     button.connect_clicked(move |_| {
-        info!("Steam AiO button clicked");
+        info!("Gaming Suite button clicked");
 
-        let commands = CommandSequence::new()
-            .then(
+        let mut commands = CommandSequence::new();
+
+        // Check if CachyOS gaming packages are available in repos
+        let cachy_meta_available = crate::core::is_package_in_repos("cachyos-gaming-meta");
+        let cachy_apps_available = crate::core::is_package_in_repos("cachyos-gaming-applications");
+
+        if cachy_meta_available && cachy_apps_available {
+            info!("CachyOS gaming packages found in repos, installing from repos");
+            commands = commands.then(
+                Command::builder()
+                    .privileged()
+                    .program("pacman")
+                    .args(&[
+                        "-S",
+                        "--noconfirm",
+                        "--needed",
+                        "cachyos-gaming-meta",
+                        "cachyos-gaming-applications",
+                    ])
+                    .description("Installing CachyOS gaming meta packages...")
+                    .build(),
+            );
+        } else {
+            info!("CachyOS gaming packages not in repos, installing curated gaming suite");
+
+            // -- Step 1: Multilib runtime libraries & Wine/Proton stack --
+            commands = commands.then(
+                Command::builder()
+                    .privileged()
+                    .program("pacman")
+                    .args(&[
+                        "-S",
+                        "--noconfirm",
+                        "--needed",
+                        // Audio
+                        "alsa-plugins",
+                        "lib32-alsa-plugins",
+                        // Media/codec libs
+                        "giflib",
+                        "lib32-giflib",
+                        "gst-plugins-base-libs",
+                        "lib32-gst-plugins-base-libs",
+                        "libjpeg-turbo",
+                        "lib32-libjpeg-turbo",
+                        "mpg123",
+                        "lib32-mpg123",
+                        "libxslt",
+                        "openal",
+                        "lib32-openal",
+                        // Video acceleration
+                        "libva",
+                        "lib32-libva",
+                        // OpenCL
+                        "opencl-icd-loader",
+                        "lib32-opencl-icd-loader",
+                        // Vulkan
+                        "vulkan-icd-loader",
+                        "lib32-vulkan-icd-loader",
+                        "vulkan-tools",
+                        // GTK (needed by some launchers/games)
+                        "lib32-gtk3",
+                        // GLFW
+                        "glfw",
+                        // Fonts (required by many Windows games)
+                        "ttf-liberation",
+                        "wqy-zenhei",
+                        // Wine & Proton tools
+                        "wine-staging",
+                        "winetricks",
+                        "protontricks",
+                        "umu-launcher",
+                        // Gaming tools & launchers
+                        "steam",
+                        "gamescope",
+                        "gamemode",
+                        "lib32-gamemode",
+                        "mangohud",
+                        "lib32-mangohud",
+                        "goverlay",
+                        "lutris",
+                    ])
+                    .description("Installing gaming libraries, Wine, and tools from repos...")
+                    .build(),
+            );
+
+            // -- Step 2: AUR packages --
+            commands = commands.then(
                 Command::builder()
                     .aur()
                     .args(&[
                         "-S",
                         "--noconfirm",
                         "--needed",
-                        "steam",
-                        "gamescope",
-                        "mangohud",
-                        "mangoverlay",
-                        "protonplus",
-                        "lib32-mangohud",
-                        "wine-meta",
-                        "wine-nine",
-                        "ttf-liberation",
-                        "lib32-fontconfig",
-                        "wqy-zenhei",
-                        "vkd3d",
-                        "giflib",
-                        "lib32-giflib",
-                        "libpng",
-                        "lib32-libpng",
-                        "libldap",
-                        "lib32-libldap",
-                        "gnutls",
-                        "lib32-gnutls",
-                        "mpg123",
-                        "lib32-mpg123",
-                        "openal",
-                        "lib32-openal",
-                        "v4l-utils",
-                        "lib32-v4l-utils",
-                        "libpulse",
-                        "lib32-libpulse",
-                        "libgpg-error",
-                        "lib32-libgpg-error",
-                        "alsa-plugins",
-                        "lib32-alsa-plugins",
-                        "alsa-lib",
-                        "lib32-alsa-lib",
-                        "libjpeg-turbo",
-                        "lib32-libjpeg-turbo",
-                        "sqlite",
-                        "lib32-sqlite",
-                        "libxcomposite",
-                        "lib32-libxcomposite",
-                        "libxinerama",
-                        "lib32-libgcrypt",
-                        "libgcrypt",
-                        "lib32-libxinerama",
-                        "ncurses",
-                        "lib32-ncurses",
-                        "ocl-icd",
-                        "lib32-ocl-icd",
-                        "libxslt",
-                        "lib32-libxslt",
-                        "libva",
-                        "lib32-libva",
-                        "gtk3",
-                        "lib32-gtk3",
-                        "gst-plugins-base-libs",
-                        "lib32-gst-plugins-base-libs",
-                        "vulkan-icd-loader",
-                        "lib32-vulkan-icd-loader",
-                        "cups",
-                        "dosbox",
-                        "lib32-opencl-icd-loader",
-                        "lib32-vkd3d",
-                        "opencl-icd-loader",
+                        "heroic-games-launcher-bin",
                     ])
-                    .description("Installing Steam and gaming dependencies...")
+                    .description("Installing Heroic Games Launcher from AUR...")
                     .build(),
-            )
-            .build();
+            );
 
-        task_runner::run(window.upcast_ref(), commands, "Steam AiO Installation");
+            // -- Step 3: Splitlock mitigation disable (gaming perf optimization) --
+            commands = commands.then(
+                Command::builder()
+                    .privileged()
+                    .program("sh")
+                    .args(&[
+                        "-c",
+                        "echo 'kernel.split_lock_mitigate=0' > /etc/sysctl.d/99-splitlock.conf && sysctl --system",
+                    ])
+                    .description("Disabling split-lock mitigation for gaming performance...")
+                    .build(),
+            );
+        }
+
+        task_runner::run(window.upcast_ref(), commands.build(), "Gaming Suite Installation");
     });
 }
 
@@ -138,66 +172,6 @@ fn setup_lact_oc(builder: &Builder, window: &ApplicationWindow) {
             .build();
 
         task_runner::run(window.upcast_ref(), commands, "LACT GPU Tools");
-    });
-}
-
-fn setup_lutris(builder: &Builder, window: &ApplicationWindow) {
-    let button = extract_widget::<Button>(builder, "btn_lutris");
-    let window = window.clone();
-
-    button.connect_clicked(move |_| {
-        info!("Lutris button clicked");
-
-        let commands = CommandSequence::new()
-            .then(
-                Command::builder()
-                    .normal()
-                    .program("flatpak")
-                    .args(&[
-                        "install",
-                        "-y",
-                        "net.lutris.Lutris",
-                        "org.freedesktop.Platform.VulkanLayer.gamescope/x86_64/25.08",
-                        "org.freedesktop.Platform.VulkanLayer.MangoHud/x86_64/25.08",
-                    ])
-                    .description("Installing Lutris and Vulkan layers...")
-                    .build(),
-            )
-            .build();
-
-        task_runner::run(window.upcast_ref(), commands, "Lutris Installation");
-    });
-}
-
-fn setup_heroic(builder: &Builder, window: &ApplicationWindow) {
-    let button = extract_widget::<Button>(builder, "btn_heroic");
-    let window = window.clone();
-
-    button.connect_clicked(move |_| {
-        info!("Heroic button clicked");
-
-        let commands = CommandSequence::new()
-            .then(
-                Command::builder()
-                    .normal()
-                    .program("flatpak")
-                    .args(&[
-                        "install",
-                        "-y",
-                        "com.heroicgameslauncher.hgl",
-                        "org.freedesktop.Platform.VulkanLayer.gamescope/x86_64/25.08",
-                        "org.freedesktop.Platform.VulkanLayer.MangoHud/x86_64/25.08",
-                    ])
-                    .description("Installing Heroic Games Launcher...")
-                    .build(),
-            )
-            .build();
-
-        task_runner::run(
-            window.upcast_ref(),
-            commands,
-            "Heroic Launcher Installation",
-        );
     });
 }
 
@@ -273,21 +247,83 @@ fn setup_falcond(builder: &Builder, window: &ApplicationWindow) {
     button.connect_clicked(move |_| {
         info!("Falcond button clicked");
 
-        let commands = CommandSequence::new()
-            .then(
+        let mut commands = CommandSequence::new();
+        
+        // Remove power-profiles-daemon if installed (conflicts with tuned-ppd)
+        if crate::core::is_package_installed("power-profiles-daemon") {
+            info!("power-profiles-daemon installed, removing first (conflicts with tuned-ppd)");
+            commands = commands.then(
+                Command::builder()
+                    .privileged()
+                    .program("sh")
+                    .args(&["-c", "pacman -Rns --noconfirm power-profiles-daemon || true"])
+                    .description("Removing power-profiles-daemon (conflicts with tuned-ppd)...")
+                    .build(),
+            );
+        }
+        
+        // Packages to install
+        let repo_candidates = ["falcond", "falcond-gui", "tuned-ppd"];
+        
+        let mut pacman_packages: Vec<&str> = Vec::new();
+        let mut aur_packages: Vec<&str> = Vec::new();
+        let mut all_in_repos = true;
+        
+        for pkg in repo_candidates {
+            // Skip if already installed
+            if crate::core::is_package_installed(pkg) {
+                info!("{} already installed, skipping", pkg);
+                continue;
+            }
+            
+            // Check if available in repos
+            if crate::core::is_package_in_repos(pkg) {
+                info!("{} found in repos", pkg);
+                pacman_packages.push(pkg);
+            } else {
+                info!("{} not in repos, will use AUR", pkg);
+                aur_packages.push(pkg);
+                all_in_repos = false;
+            }
+        }
+        
+        // If any package needs AUR, add falcond-profiles too (AUR-only)
+        if !all_in_repos && !crate::core::is_package_installed("falcond-profiles") {
+            info!("falcond-profiles not installed, adding to AUR list");
+            aur_packages.push("falcond-profiles");
+        }
+        
+        // Install from repos first
+        if !pacman_packages.is_empty() {
+            let mut args = vec!["-S", "--noconfirm", "--needed"];
+            args.extend(pacman_packages.iter());
+            
+            commands = commands.then(
+                Command::builder()
+                    .privileged()
+                    .program("pacman")
+                    .args(&args)
+                    .description("Installing Falcond packages from repos...")
+                    .build(),
+            );
+        }
+        
+        // Install remaining from AUR (only if needed)
+        if !aur_packages.is_empty() {
+            let mut args = vec!["-S", "--noconfirm", "--needed"];
+            args.extend(aur_packages.iter());
+            
+            commands = commands.then(
                 Command::builder()
                     .aur()
-                    .args(&[
-                        "-S",
-                        "--noconfirm",
-                        "--needed",
-                        "falcond",
-                        "falcond-gui",
-                        "falcond-profiles",
-                    ])
-                    .description("Installing Falcond Gaming utility...")
+                    .args(&args)
+                    .description("Installing Falcond packages from AUR...")
                     .build(),
-            )
+            );
+        }
+        
+        // Post-install setup (always run to ensure proper configuration)
+        commands = commands
             .then(
                 Command::builder()
                     .privileged()
@@ -317,7 +353,7 @@ fn setup_falcond(builder: &Builder, window: &ApplicationWindow) {
                     .privileged()
                     .program("chown")
                     .args(&[":falcond", "/usr/share/falcond/profiles/user"])
-                    .description("Adding propper ownership permissions...")
+                    .description("Adding proper ownership permissions...")
                     .build(),
             )
             .then(
@@ -325,7 +361,7 @@ fn setup_falcond(builder: &Builder, window: &ApplicationWindow) {
                     .privileged()
                     .program("chmod")
                     .args(&["2775", "/usr/share/falcond/profiles/user"])
-                    .description("Adding propper executable permissions...")
+                    .description("Adding proper executable permissions...")
                     .build(),
             )
             .then(
@@ -335,9 +371,8 @@ fn setup_falcond(builder: &Builder, window: &ApplicationWindow) {
                     .args(&["enable", "--now", "falcond"])
                     .description("Enabling falcond background service...")
                     .build(),
-            )
-            .build();
+            );
 
-        task_runner::run(window.upcast_ref(), commands, "Falcond Installation");
+        task_runner::run(window.upcast_ref(), commands.build(), "Falcond Installation");
     });
 }
